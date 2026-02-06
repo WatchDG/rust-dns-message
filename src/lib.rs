dbf1abad62c11579bf29363e856c66ed67b5e8ac
WatchDG <baranov@w3m.co>
2026-02-07 01:57:17 +0300

    Update version to 0.1.1 in Cargo.toml and Cargo.lock; add new Header::new constructor for easier Header instantiation.

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,23 @@ pub struct Header {
     pub ns_count: u16,
     pub ar_count: u16,
 }
+
+impl Header {
+    pub fn new(
+        id: u16,
+        flags: u16,
+        qd_count: u16,
+        an_count: u16,
+        ns_count: u16,
+        ar_count: u16,
+    ) -> Self {
+        Self {
+            id,
+            flags,
+            qd_count,
+            an_count,
+            ns_count,
+            ar_count,
+        }
+    }
+}
