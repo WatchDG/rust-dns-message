f9cbe2ed008d771a7b26c6ba57baac93b0313e17
WatchDG <baranov@w3m.co>
2026-02-07 12:38:11 +0300

    Rename `question` field to `questions` in Message struct for clarity; update constructor accordingly.

--- src/message.rs
+++ src/message.rs
@@ -3,11 +3,11 @@ use crate::{Header, Question};
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Message<'a> {
     pub header: Header,
-    pub question: &'a [Question<'a>],
+    pub questions: &'a [Question<'a>],
 }
 
 impl<'a> Message<'a> {
-    pub fn new(header: Header, question: &'a [Question<'a>]) -> Self {
-        Self { header, question }
+    pub fn new(header: Header, questions: &'a [Question<'a>]) -> Self {
+        Self { header, questions }
     }
 }
