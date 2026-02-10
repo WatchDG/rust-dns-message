8559b17d177abb0c1678d1d4cc4f1a1975f3ec55
WatchDG <baranov@w3m.co>
2026-02-10 19:53:13 +0300

    Refactor Question struct to use QType and QClass enums for q_type and q_class fields, enhancing type safety and clarity. Introduce QType and QClass enums with methods for conversion between enum values and their byte representations, improving DNS message handling.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod traits;
 
 pub use header::Header;
 pub use message::Message;
-pub use question::Question;
+pub use question::{QClass, QType, Question};
 use resource_record::ResourceRecord;
 
 pub type Answer<'a> = ResourceRecord<'a>;
