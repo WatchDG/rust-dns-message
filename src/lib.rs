93ea2243572de2d8a6a43e04f20084edc38113f7
WatchDG <baranov@w3m.co>
2026-02-11 08:49:23 +0300

    Update Question module to include Label, QName, and QNameKind types, enhancing type safety and clarity in DNS message handling.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod traits;
 
 pub use header::Header;
 pub use message::Message;
-pub use question::{QClass, QType, Question};
+pub use question::{Label, QClass, QName, QNameKind, QType, Question};
 use resource_record::ResourceRecord;
 
 pub type Answer<'a> = ResourceRecord<'a>;
